899fe3a0a82ac11e8e2ca4112f2099d84591554d
Jason Petterson <jp@brokenshotgun.com>
2018-04-21 00:30:06 -0400

    added player that can move and punch, added basic layout and passenger
    A little wonky, but a good start

---
+++ Assets/FighterFlightSim/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour {
+
+    private Animator animator;
+
+    void Start() {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    void Update () {
+        HandleInput();
+	}
+
+    private bool punchToggle = false;
+    private void HandleInput() {
+        if (CrossPlatformInputManager.GetButtonDown("Fire1")) {
+            animator.SetTrigger(punchToggle ? "PunchLeft" : "PunchRight");
+            punchToggle = !punchToggle;
+        }
+    }
+}
