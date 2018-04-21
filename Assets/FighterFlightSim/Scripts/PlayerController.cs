d068bc890e4438a6c1019a62f958f7c8218bab0a
Jason Petterson <jp@brokenshotgun.com>
2018-04-21 13:40:27 -0400

    added plane interior, music, added punch hit detection and ragdoll effect

--- Assets/FighterFlightSim/Scripts/PlayerController.cs
+++ Assets/FighterFlightSim/Scripts/PlayerController.cs
@@ -6,9 +6,11 @@ using UnityStandardAssets.CrossPlatformInput;
 public class PlayerController : MonoBehaviour {
 
     private Animator animator;
+    private Camera playerCamera;
 
     void Start() {
         animator = GetComponentInChildren<Animator>();
+        playerCamera = GetComponentInChildren<Camera>();
     }
 
     void Update () {
@@ -17,9 +19,45 @@ public class PlayerController : MonoBehaviour {
 
     private bool punchToggle = false;
     private void HandleInput() {
-        if (CrossPlatformInputManager.GetButtonDown("Fire1")) {
-            animator.SetTrigger(punchToggle ? "PunchLeft" : "PunchRight");
-            punchToggle = !punchToggle;
+        if (CrossPlatformInputManager.GetButton("Fire1")) {
+            Punch();
         }
+    }
+
+    [SerializeField] private float punchCooldownTime = 0.3f;
+    [SerializeField] private LayerMask punchable;
+    [SerializeField] private float punchForce = 10f;
+
+    private bool punchReady = true;
+    private RaycastHit[] punchHitResults = new RaycastHit[16];
+
+    private void Punch() {
+        if (!punchReady) return;
+
+        int hitCount = Physics.SphereCastNonAlloc(
+            playerCamera.transform.position,
+            0.5f,
+            playerCamera.transform.forward,
+            punchHitResults,
+            5f,
+            punchable);
+
+        if (hitCount > 0) {
+            punchHitResults[0].transform.GetComponentInParent<Animator>().enabled = false;
+            for (int i=0; i<hitCount; ++i) {
+                Debug.Log("Hit: " + punchHitResults[i].transform.name);
+                punchHitResults[i].rigidbody.AddForceAtPosition(playerCamera.transform.forward * punchForce, punchHitResults[i].point, ForceMode.Impulse);
+            }
+        }
+
+        animator.SetTrigger(punchToggle ? "PunchLeft" : "PunchRight");
+        punchToggle = Random.Range(0, 2) == 0;
+        punchReady = false;
+
+        Invoke("ResetPunchCooldown", punchCooldownTime);
+    }
+    
+    private void ResetPunchCooldown() {
+        punchReady = true;
     }
 }
